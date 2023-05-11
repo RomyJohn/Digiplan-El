5ab5a525829aae1f5b5c4aae08ba514eb62abbc6
romy john <romyjohn12.rj@gmail.com>
2023-05-11 10:45:42 +0530

    initial commit

---
+++ src/main/java/com/digiplan/DigiPlanApplication.java
@@ -0,0 +1,20 @@
+package com.digiplan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class DigiplanApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(DigiplanApplication.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(DigiplanApplication.class, args);
+    }
+
+}
